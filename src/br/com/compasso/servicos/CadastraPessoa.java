30778f0e2453fb1720d69c77dc63dd94669f7c83
William Silveira <williamas@mx2.unisc.br>
2020-01-16 11:37:50 -0300

    correção de mensagem de errro

--- src/br/com/compasso/servicos/CadastraPessoa.java
+++ src/br/com/compasso/servicos/CadastraPessoa.java
@@ -7,18 +7,15 @@ import java.io.IOException;
 import br.com.compasso.model.Pessoa;
 
 public class CadastraPessoa  implements EscreveArquivo{
-	
+	ErrorMessages erro = new ErrorMessages();
 	@Override
 	public void registra(Pessoa p, String filename) {
 		try {
-
 			BufferedWriter bw =new BufferedWriter(new FileWriter(filename, true));
 			salvaPessoa(p, bw);
-			
 		} catch (Exception e) {
-			
+			this.erro.errorMessage("Algum erro ocorreu no cadastramento, tente novamente");
 		}
-			
 	}
 	
 	private void salvaPessoa(Pessoa p, BufferedWriter bw) throws IOException {
